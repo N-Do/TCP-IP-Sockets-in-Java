cf03ad04fd30d4f45e8db5a4dfe6ef6f1c20da2b
N-Do <nd.1382124@gmail.com>
2017-06-16 13:39:34 -0400

    Added InetAddressExample and Sandbox

---
+++ src/Sandbox.java
@@ -0,0 +1,11 @@
+import java.net.*;
+
+public class Sandbox {
+	public static void main(String[] args)
+	{
+		String[] servers = {"www.mkp.com", "google.com", "blah.blah"};
+		InetAddressExample test = new InetAddressExample();
+		
+		test.main(servers);
+	}
+}
